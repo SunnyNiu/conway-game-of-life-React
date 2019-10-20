4a0be078bd53656d0487c16a576fd0ad3e318867
Sunny Niu <sunnyyingli@gmail.com>
2019-10-20 19:42:57 +1300

    Updates

---
+++ precache-manifest.44705cf7f94e8247022ebfdc3064f24e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a0dc126d8d43cbb9e1084e58e6a499a",
+    "url": "/cornways-react/index.html"
+  },
+  {
+    "revision": "053ef0956d21d6e13ac8",
+    "url": "/cornways-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6eb8484bf529ad7699f2",
+    "url": "/cornways-react/static/js/2.c9cf2b96.chunk.js"
+  },
+  {
+    "revision": "053ef0956d21d6e13ac8",
+    "url": "/cornways-react/static/js/main.8f4631c9.chunk.js"
+  },
+  {
+    "revision": "cc6e7a0f555553fe7c68",
+    "url": "/cornways-react/static/js/runtime-main.d9e64491.js"
+  }
+]);
