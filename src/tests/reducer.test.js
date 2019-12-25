4c5ae56b64f96b0be73443edcd05b6a3b8cd2a56
Sunny Niu <sunnyyingli@gmail.com>
2019-12-25 19:23:11 +1300

    add reducer test cases

---
+++ src/tests/reducer.test.js
@@ -0,0 +1,132 @@
+import {
+  newBoard,
+  startGame,
+  stopGame
+  // setSelectedType,
+  // setSpeed
+} from '../redux/action'
+
+import gameReducer from '../redux/reducer'
+
+describe('gamereducer tests', () => {
+  it('newBoard action sets the new board', () => {
+    const currentState = {
+      matrix: [[false, false, false], [false, false, false], [false, false, false]],
+      size: 3,
+      selected: '',
+      isRunning: false,
+      speed: 1
+    }
+
+    const board = [[false, false], [false, false]]
+    const action = {
+      type: 'SET_NEW_BOARD',
+      payload: board
+    }
+    const expected = {
+      matrix: [[false, false], [false, false]],
+      size: 3,
+      selected: '',
+      isRunning: false,
+      speed: 1
+    }
+    const actual = gameReducer(currentState, action)
+    expect(actual).toEqual(expected)
+  })
+
+  it('startGame action sets isRunning to true', () => {
+    const currentState = {
+      matrix: [[false, false, false], [false, false, false], [false, false, false]],
+      size: 3,
+      selected: '',
+      isRunning: false,
+      speed: 1
+    }
+
+    const action = {
+      type: 'START_GAME'
+    }
+    const expected = {
+      matrix: [[false, false, false], [false, false, false], [false, false, false]],
+      size: 3,
+      selected: '',
+      isRunning: true,
+      speed: 1
+    }
+    const actual = gameReducer(currentState, action)
+    expect(actual).toEqual(expected)
+  })
+
+  it('stopGame action sets isRunning to false', () => {
+    const currentState = {
+      matrix: [[false, false, false], [false, false, false], [false, false, false]],
+      size: 3,
+      selected: '',
+      isRunning: false,
+      speed: 1
+    }
+
+    const action = {
+      type: 'STOP_GAME'
+    }
+    const expected = {
+      matrix: [[false, false, false], [false, false, false], [false, false, false]],
+      size: 3,
+      selected: '',
+      isRunning: false,
+      speed: 1
+    }
+    const actual = gameReducer(currentState, action)
+    expect(actual).toEqual(expected)
+  })
+
+  it('setSeletedType action sets pattern to selected type', () => {
+    const currentState = {
+      matrix: [[false, false, false], [false, false, false], [false, false, false]],
+      size: 3,
+      selected: '',
+      isRunning: false,
+      speed: 1
+    }
+
+    const type = 'Glider'
+    const action = {
+      type: 'SET_SELECTED_TYPE',
+      payload: type
+    }
+    const expected = {
+      matrix: [[false, false, false], [false, false, false], [false, false, false]],
+      size: 3,
+      selected: 'Glider',
+      isRunning: false,
+      speed: 1
+    }
+    const actual = gameReducer(currentState, action)
+    expect(actual).toEqual(expected)
+  })
+
+  it('setSpeed action sets speed to specific speed', () => {
+    const currentState = {
+      matrix: [[false, false, false], [false, false, false], [false, false, false]],
+      size: 3,
+      selected: '',
+      isRunning: false,
+      speed: 1
+    }
+
+    const speed = 5
+    const action = {
+      type: 'SET_SPEED',
+      payload: speed
+    }
+    const expected = {
+      matrix: [[false, false, false], [false, false, false], [false, false, false]],
+      size: 3,
+      selected: '',
+      isRunning: false,
+      speed: 5
+    }
+    const actual = gameReducer(currentState, action)
+    expect(actual).toEqual(expected)
+  })
+})
